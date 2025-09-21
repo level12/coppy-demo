76b3c6f6751a0f91e0a3a318a05f4db04e5df903
Randy Syring <randy.syring@level12.io>
2025-09-20 22:28:04 -0400

    Additional upstream changes

--- noxfile.py
+++ noxfile.py
@@ -7,40 +7,6 @@ package_path = Path(__file__).parent
 nox.options.default_venv_backend = 'uv'
 
 
-def pytest_run(session: nox.Session, *args, **env):
-    session.run(
-        'pytest',
-        '-ra',
-        '--tb=native',
-        '--strict-markers',
-        '--cov',
-        '--cov-config=.coveragerc',
-        f'--junit-xml={package_path}/ci/test-reports/{session.name}.pytests.xml',
-        f'--cov-report=xml:{package_path}/ci/coverage/{session.name}.xml',
-        '--no-cov-on-fail',
-        package_path / 'tests',
-        *args,
-        *session.posargs,
-        env=env,
-    )
-
-
-def uv_sync(session: nox.Session, *groups, project=False, extra=None):
-    project_args = () if project or session.name.startswith('pytest') else ('--no-install-project',)
-    group_args = [arg for group in groups for arg in ('--group', group)]
-    extra_args = ('--extra', extra) if extra else ()
-    run_args = (
-        'uv',
-        'sync',
-        '--active',
-        '--no-default-groups',
-        *project_args,
-        *group_args,
-        *extra_args,
-    )
-    session.run(*run_args)
-
-
 @nox.session
 def pytest(session: nox.Session):
     uv_sync(session, 'pytest')
@@ -65,4 +31,40 @@ def audit(session: nox.Session):
         'pip-audit',
         '--desc',
         '--skip-editable',
+    )
+
+
+def uv_sync(session: nox.Session, *groups, project=False, extra=None):
+    if not groups:
+        groups = (session.name,)
+    project_args = () if project or session.name.startswith('pytest') else ('--no-install-project',)
+    group_args = [arg for group in groups for arg in ('--group', group)]
+    extra_args = ('--extra', extra) if extra else ()
+    run_args = (
+        'uv',
+        'sync',
+        '--active',
+        '--no-default-groups',
+        *project_args,
+        *group_args,
+        *extra_args,
+    )
+    session.run(*run_args)
+
+
+def pytest_run(session: nox.Session, *args, **env):
+    session.run(
+        'pytest',
+        '-ra',
+        '--tb=native',
+        '--strict-markers',
+        '--cov',
+        '--cov-config=.coveragerc',
+        f'--junit-xml={package_path}/ci/test-reports/{session.name}.pytests.xml',
+        f'--cov-report=xml:{package_path}/ci/coverage/{session.name}.xml',
+        '--no-cov-on-fail',
+        package_path / 'tests',
+        *args,
+        *session.posargs,
+        env=env,
     )
